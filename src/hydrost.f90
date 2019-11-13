6db098b1f91f2e1eb7426b5ba19dccf1eb16c35e
Daniel Lo <danielloyw@gmail.com>
2019-11-13 15:43:32 -0700

    Corrected hydrost to actually enforce hydrostatic equilibrium
    Only output pressures were changed previously, resulting in inconsistencies

--- src/hydrost.f90
+++ src/hydrost.f90
@@ -12,7 +12,7 @@ SUBROUTINE HYDROST
 
   INTEGER :: nz,nm
   REAL(RP) :: alp, bet, fac
-  REAL(RP), ALLOCATABLE, DIMENSION(:) :: y, pnew,xnew
+  REAL(RP), ALLOCATABLE, DIMENSION(:) :: y, pnew, xnew
 !  CHARACTER(len=1) :: iret
 
   ALLOCATE(y(nlev),pnew(nlev),xnew(nlev))
@@ -23,48 +23,48 @@ SUBROUTINE HYDROST
 
   DO nz = 1, nlev
      y(nz) = RPLANET/rz(nz) - one
+     prs(nz) = rkb*tn(nz)*den(nz,0)
   END DO
-
-  
 
   pnew(1) = rkb*tn(1)*den(1,0)
   DO nz = 2, nlev
-     alp = mass(nz-1)/tn(nz-1)
-     bet = (mass(nz)/tn(nz) - alp)
-     fac = (GM*amu/rkb/RPLANET)*(y(nz)-y(nz-1))*(mass(nz)/tn(nz)+mass(nz-1)/tn(nz-1))/two
-!     fac = (GM*amu/rkb/RPLANET)*(alp*(y(nz)-y(nz-1))+half*bet*(y(nz)+y(nz-1))) !GM/kT/R*dy*m
+     fac = (GM*amu/rkb/RPLANET)*(y(nz)-y(nz-1))*(mass(nz)/tn(nz)+mass(nz-1)/tn(nz-1))/two !GM/kT/R*dy*m
      pnew(nz) = pnew(nz-1)*EXP(fac)
   END DO
 
 !  DO nz = 1, nlev
-!     WRITE(*,"(F8.0,2X,F8.3,2(2X,ES11.3))") 1.E-5*z(nz),mass(nz),prs(nz),pnew(nz)
+!     IF(abs(pnew(nz)-prs(nz))/prs(nz) > 1.E-3_RP) THEN
+!        WRITE(*,"(' HYDROST: nz, prs, pnew ',I6,2ES12.5)") nz, prs(nz), pnew(nz)
+!        STOP
+!     END IF
 !  END DO
-!  READ(*,"(A)") iret
 
   !
   !  .. Map mass, temperature and mole fractions to new pressure grid
   !
 
-!  xnew = PMAP(mass, prs, pnew)
-!  mass = xnew
+  xnew = PMAP(mass, prs, pnew)
+  mass = xnew
 !  xnew = PMAP(tn, prs, pnew)
 !  tn = xnew
 !  xnew = PMAP(te, prs, pnew)
 !  te = xnew
-!  DO nm = 1, nsp
-!     xnew = PMAP(xmol(:,nm), prs, pnew)
-!     xmol(1:nlev,nm) = xnew(1:nlev)
-!  END DO
+  DO nm = 1, nsp
+     xnew = PMAP(xmol(:,nm), prs, pnew)
+     xmol(1:nlev,nm) = xnew(1:nlev)
+  END DO
 
-!  DO nz = 1, nlev
-!     xmol(nz,1) = one-SUM(xmol(nz,2:nsp))
-!  END DO
+  DO nz = 1, nlev
+     xmol(nz,1) = one-SUM(xmol(nz,2:nsp))
+  END DO
+
 
   prs(1:nlev) = pnew(1:nlev)
-!  den(1:nlev,0) = prs(1:nlev)/rkb/tn(1:nlev)
-!  DO nm = 1, nsp
-!     den(1:nlev,nm) = xmol(1:nlev,nm)*den(1:nlev,0)
-!  END DO
+  den(1:nlev,0) = prs(1:nlev)/rkb/tn(1:nlev)
+  DO nm = 1, nsp
+     den(1:nlev,nm) = xmol(1:nlev,nm)*den(1:nlev,0)
+  END DO
+  
 
   DEALLOCATE(y,pnew,xnew)
 
@@ -72,7 +72,7 @@ SUBROUTINE HYDROST
 
 CONTAINS
 
-  FUNCTION PMAP(ytab,ptab,prs)
+  FUNCTION PMAP(ytab,ptab,prs) ! interpolates ytab values at ptab pressures to new prs pressures
     USE PRECISION
     USE CONSTANTS
     USE SUBS, ONLY : LOCATE
