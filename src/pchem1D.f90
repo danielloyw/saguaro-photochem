ed8953e035279c51646227a93d94cffeb29c7e24
Daniel Lo <danielloyw@gmail.com>
2019-11-14 16:57:33 -0700

    Update pchem1D.f90
    1) Removed redundant rate recalculations
    2) Added booleans for running soldep and eldep
    3) Shifted xmol, rho and mass calculations to after chem loop (as it should be)

--- src/pchem1D.f90
+++ src/pchem1D.f90
@@ -9,14 +9,13 @@ PROGRAM PCHEM1D
 
   !  .. Local Variable Declarations
 
-  IMPLICIT none 
+  IMPLICIT none
 
   REAL(RP) :: tol_diff, tol_chem, test_chem, test_diff, test_time
   INTEGER :: ntim_diff, ntim_chem, iter_chem, iter_diff ! max diffusion time steps (overall number of loops), max reaction time steps per diffusion step, iteration count for reactions, iteration count for diffusion
   INTEGER :: nm, nl, nx, np, nln, nmn, nm1, nm2, nm3, nm4, nm5
   INTEGER :: isol, iprnt
   REAL(RP) :: dnm, sm, tst1
-  CHARACTER(len=1) :: iret
 
   ! #################################################################################################
   ! #                                                                                               #
@@ -24,6 +23,8 @@ PROGRAM PCHEM1D
   ! #                                                                                               #
   ! #################################################################################################
 
+  lcompo_sol = .false.
+  lchemeq_sol = .false.
 
   tol_diff = 1.E-10_RP
   tol_chem = 1.E-10_RP
@@ -39,7 +40,7 @@ PROGRAM PCHEM1D
      READ(20,"(A)") runid
   CLOSE(unit=20)
 
-!  WRITE(*,"(' Enter time step and # of steps ........  > ')", ADVANCE='NO') 
+!  WRITE(*,"(' Enter time step and # of steps ........  > ')", ADVANCE='NO')
 !  READ(*,*) tstep_diff, ntim_diff
 
   OPEN(unit=21,file='tctl.out',status='old',action='read')
@@ -53,7 +54,7 @@ PROGRAM PCHEM1D
      READ(60,*) cos_sza
      READ(60,*) diurnal_average
   CLOSE(unit=60)
-  
+
   !   .. Read in parameters for run
 
   OPEN(unit=60,file='pchem1D.ctl',status='old',action='read')
@@ -71,7 +72,7 @@ PROGRAM PCHEM1D
      READ(60,*) iprnt, isol ! frequency to write results to disc, frequency to recalculate photolysis and photoelectrons
   CLOSE(unit=60)
 
-  
+
   ! #################################################################################################
   ! #                                                                                               #
   ! #                                     PREPARE CALCULATIONS                                      #
@@ -79,37 +80,34 @@ PROGRAM PCHEM1D
   ! #################################################################################################
 
   CALL READ_MOLECULES           !  Read in molecule data
-  
+
   CALL READ_REACTIONS           !  Read in reaction rate data
-  
+
   CALL READ_ATMOS               !  Read model atmosphere
-  
+
   CALL DIFCO                    !  Calculate diffusion coefficients
-  
+
   CALL PHOTO                    !  Set up photolyis calculation
-  
+
   CALL ELCTRN                   !  Set up suprathermal electron calculation
- 
+
   CALL READ_AEROSOL             !  Read Aerosol Model
-  
+
   CALL READ_RAYLEIGH            !  Calculate Rayleigh optical depth
-  
+
   CALL PATHS1D                  !  Calculate path lengths for solar absorption
-  
+
   CALL RATECO                   !  Calculate reaction rate coeff
-  
+
   CALL EXT_PROD                 !  Specified production rates
 
-  ALLOCATE(pr_ph(nlev,0:nsp),ls_ph(nlev,0:nsp),     & !  Production and Loss from photon processes   
-           pr_pe(nlev,0:nsp),ls_pe(nlev,0:nsp),     & !  Production and Loss from electron processes  
+  ALLOCATE(pr_ph(nlev,0:nsp),ls_ph(nlev,0:nsp),     & !  Production and Loss from photon processes
+           pr_pe(nlev,0:nsp),ls_pe(nlev,0:nsp),     & !  Production and Loss from electron processes
            pr_chem(nlev,0:nsp),ls_chem(nlev,0:nsp), & !  Production and Loss from chemical processes
            pr(nlev,0:nsp),ls(nlev,0:nsp),           & !  Net production and loss
            div_flx(nlev,0:nsp),dNdt(nlev,0:nsp),    & !  Flux divergence and net balance
            rcdn(nlev,0:nsp),flx(nlev,0:nsp))          !  Condensation loss and flux
 
-!  CALL SOLDEP1 
-
-!  CALL COMPOUT
 
   ! #################################################################################################
   ! #                                                                                               #
@@ -121,9 +119,8 @@ PROGRAM PCHEM1D
   WRITE(*,"(' START TIME LOOP')")
   iter_diff = 0; test_diff = 10._RP*tol_diff
   TIME: DO
-     
+
      iter_diff = iter_diff + 1
-!     IF((iter_diff > ntim_diff) .or. (test_diff < tol_diff)) EXIT
      IF(iter_diff > ntim_diff) EXIT
 
      den_old(1:nlev,0:nsp) = den(1:nlev,0:nsp)
@@ -131,109 +128,54 @@ PROGRAM PCHEM1D
      IF (MOD(iter_diff-1,isol) == 0) THEN
 
         !  .. Photolysis Reactions
- 
+
+     !  Occasionally calculate photolysis rate using high resolution in region B (lcrsB = .true.)
         lcrsA = .true.; lcrsB = .true.; lcrsC = .true.; lcrsJ = .true.
 
         CALL SOLDEP1
 
         !  .. Suprathermal Electron Reactions
-        
+
         CALL ELDEP1
 
      END IF
 
-     !  .. Photon production and loss
-     
-     pr_ph = zero
-     ls_ph = zero
-     rpt = zero
-     DO np = 1, nphrt
-        nm1=irpt(1,np)
-        nm2=irpt(2,np)
-        nm3=irpt(3,np)
-        nm4=irpt(4,np)
-        nm5=irpt(5,np)
-        DO nl = 1, nlev
-           rpt(np,nl) = rph(np,nl)*den(nl,nm1)
-           ls_ph(nl,nm1) = ls_ph(nl,nm1) + rpt(np,nl)
-           pr_ph(nl,nm2) = pr_ph(nl,nm2) + rpt(np,nl)
-           pr_ph(nl,nm3) = pr_ph(nl,nm3) + rpt(np,nl)
-           pr_ph(nl,nm4) = pr_ph(nl,nm4) + rpt(np,nl)
-           pr_ph(nl,nm5) = pr_ph(nl,nm5) + rpt(np,nl)
-        END DO
-     END DO
-
-     !  .. Suprathermal electron production and loss
-      
-     pr_pe = zero
-     ls_pe = zero
-     rpe = zero
-     DO np = 1, nert
-        nm1=iert(1,np)
-        nm2=iert(2,np)
-        nm3=iert(3,np)
-        nm4=iert(4,np)
-        nm5=iert(5,np)
-        DO nl = nibot, nlev
-           rpe(np,nl) = eph(np,nl)*den(nl,nm1)
-           ls_pe(nl,nm1) = ls_pe(nl,nm1) + rpe(np,nl)
-           pr_pe(nl,nm2) = pr_pe(nl,nm2) + rpe(np,nl)
-           pr_pe(nl,nm3) = pr_pe(nl,nm3) + rpe(np,nl)
-           pr_pe(nl,nm4) = pr_pe(nl,nm4) + rpe(np,nl)
-           pr_pe(nl,nm5) = pr_pe(nl,nm5) + rpe(np,nl)
-        END DO
-     END DO
-
      ! ---------------------------------------------------------------------------------------------
      !
-     !                                   DIFFUSING SPECIES                                         
+     !                                   DIFFUSING SPECIES
      !
      ! ---------------------------------------------------------------------------------------------
-         
+
      lcrsA = .true.; lcrsB = .false.; lcrsC = .true.; lcrsJ = .true.
      CALL COMPO
 
-     DO nl = 1, nlev
-        den(nl,0) = SUM(den(nl,1:nsp))
-        prs(nl) = rkb*tn(nl)*den(nl,0)
-     END DO
-
-     DO nl = 1, nlev
-        IF (nionsmax > 0) THEN
-           rho(nl) = amu*DOT_PRODUCT(mmw(1:nsp),den(nl,1:nsp))
-           mass(nl) = rho(nl)/(den(nl,0)+two*den(nl,nsp))/amu
-        ELSE
-           rho(nl) = amu*DOT_PRODUCT(mmw(1:nsp),den(nl,1:nsp))
-           mass(nl) = rho(nl)/den(nl,0)/amu
-        END IF
-     END DO
-
-      CALL HYDROST
+!     CALL HYDROST
 
       ! ---------------------------------------------------------------------------------------------
      !
-     !                                 CHEMICAL EQUILIBRIUM SPECIES                                
+     !                                 CHEMICAL EQUILIBRIUM SPECIES
      !
      ! ---------------------------------------------------------------------------------------------
 
-     DO nm = 1, nsp
-        IF(istat(nm) /= 0) THEN
-           den_old(1:nlev,nm)= den(1:nlev,nm)
-        END IF
-     END DO
+     lcrsA = .true.; lcrsB = .false.; lcrsC = .true.; lcrsJ = .true.
 
      iter_chem = 0; test_chem = 10._RP*tol_chem
      CHEM: DO
-     
+
         iter_chem = iter_chem + 1
         IF((iter_chem > ntim_chem) .or. (test_chem < tol_chem)) EXIT
 
         den_old = den
-     
+
+        IF(lchemeq_sol) THEN
+           CALL SOLDEP1
+!           CALL ELDEP1
+        END IF
+
         CALL CHEMEQ
 
         !  .. Check Convergence
-  
+
         test_chem = zero; nmn = locp(1); nln = 1
         DO nx = 1, nchem
            nm = locp(nx)
@@ -253,17 +195,36 @@ PROGRAM PCHEM1D
               END IF
            END DO
         END DO
-        
-        IF (lprnt) THEN
+
+        IF (lprnt .and. (nchem > 0)) THEN
            WRITE(*,"(10X,'  CHEMEQ: ITER = ',I4,2X,A12,' Z = ',F8.1,' N = ',ES11.3,' P = ',ES11.3,' L = ',ES11.3,' dNdt = ',ES11.3,' Err = ',ES11.3,' Relerr = ',ES11.3)") &
                 iter_chem,name(nmn),1.E-5_RP*z(nln),den(nln,nmn),pr(nln,nmn), ls(nln,nmn), dNdt(nln,nmn),pr(nln,nmn)-ls(nln,nmn)-dNdt(nln,nmn),test_chem
         END IF
-       
+
      END DO CHEM
+
+     ! .. Update stuff
+
+     DO nm = 1, nsp
+        DO nl = 1, nlev
+           xmol(nl,nm) = den(nl,nm)/den(nl,0)
+        END DO
+     END DO
+
+     DO nl = 1, nlev
+        IF (nionsmax > 0) THEN
+           rho(nl) = amu*DOT_PRODUCT(mmw(1:nsp),den(nl,1:nsp))
+           mass(nl) = rho(nl)/(den(nl,0)+two*den(nl,nsp))/amu
+        ELSE
+           rho(nl) = amu*DOT_PRODUCT(mmw(1:nsp),den(nl,1:nsp))
+           mass(nl) = rho(nl)/den(nl,0)/amu
+        END IF
+     END DO
+
 
      ! ---------------------------------------------------------------------------------------------
      !
-     !                                   CHECK CONVERGENCE                                         
+     !                                   CHECK CONVERGENCE
      !
      ! ---------------------------------------------------------------------------------------------
 
@@ -286,7 +247,6 @@ PROGRAM PCHEM1D
            END DO
         END DO
      END IF
-  
 
      IF(nchem > 0) THEN
         DO nx = 1, nchem
@@ -307,20 +267,20 @@ PROGRAM PCHEM1D
      END IF
 
      WRITE(*,910) iter_diff,name(nmn),1.E-5_RP*z(nln),den(nln,nmn),den(nln,nmn)-den_old(nln,nmn),test_time
-     
+
      !  .. Write to Disc
-     
+
      IF (MOD(iter_diff-1,iprnt) == 0) THEN
         CALL COMPOUT
      END IF
-     
+
   END DO TIME
-  
-     
+
+
   CALL COMPOUT                    !  .. Final output
 
 
-910 FORMAT(10X,' STEP=',I6,' SPECIES = ',A12,' Z = ',ES12.4,' N = ',ES11.3,' DelN =',ES11.3,' Rerr = ',ES11.3) 
+910 FORMAT(10X,' STEP=',I6,' SPECIES = ',A12,' Z = ',ES12.4,' N = ',ES11.3,' DelN =',ES11.3,' Rerr = ',ES11.3)
 911 FORMAT(5X,' STEP=',I6,2X,A12,' DNDT = ',ES11.3,' Pr = ',ES11.3,' Ls = ',   &
       ES11.3,' div_flx = ',ES11.3,' Z = ',ES12.4,' Test = ',ES11.3)
 912 FORMAT(10X,A12,' DNDT = ',ES11.3,' Pr = ',ES11.3,' Ls = ',   &
@@ -328,5 +288,5 @@ PROGRAM PCHEM1D
 913 FORMAT(10X,'  CHEM : ITER=',I3,2X,' Species = ',A12,' Alt = ',F8.1, &
          ' Den = ',ES11.3' Pr = ',ES11.3,' Ls = ',ES11.3,' dNdt = ',ES11.3)
   STOP
-    
+
 END PROGRAM
