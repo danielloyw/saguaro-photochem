168ed90b401be2bf8fce1003c6be13ce2312a898
Daniel Lo <danielloyw@gmail.com>
2025-10-15 15:50:27 -0400

    Grouped read_* subroutines into read_inputs

--- mars/src/read_inputs.f90
+++ mars/src/read_inputs.f90
@@ -1,6 +1,396 @@
-module reactions_mod
+module read_inputs
 
 contains
+
+subroutine read_species
+! This subroutine reads in the settings for neutral and ion species 
+! (nmolecules.settings, imolecules.settings), then allocate the relevant
+! variables. 
+
+  !----------------------------------------------------------------------------
+  !  Modules
+  !----------------------------------------------------------------------------
+
+  use constants
+  use global_variables
+  use utils, only : find_name
+
+  !----------------------------------------------------------------------------
+  !  Local variables
+  !----------------------------------------------------------------------------
+
+  implicit none
+  ! file unit numbers for neutral and ion settings
+  integer :: fid_neu, fid_ion
+  ! number of ions in chemical equilibrium
+  integer :: ichk
+  ! number of species in chemical equilibrium + electrons
+  integer :: n_chem_e
+  
+  ! loop variables
+  integer :: i_sp, n_c, n_d
+  ! temporary / dummy variables
+  integer :: tn1
+  character(len=140) :: ts_header
+
+  !----------------------------------------------------------------------------
+  !  Read settings for neutral and ionic species
+  !----------------------------------------------------------------------------
+
+  open(newunit=fid_neu, file='nmolecules.settings', &
+    status='old', action='read')
+  open(newunit=fid_ion, file='imolecules.settings', &
+    status='old', action='read')
+
+  read(fid_neu,*) n_neu ! number of neutral species
+  read(fid_ion,*) n_ion ! number of ion species
+
+  n_sp = n_neu + n_ion + 1 ! total number of species (neutrals+ions+electrons)
+
+  ! allocate variables in preparation for population with file read
+  allocate(sp_list(0:n_sp), istat(n_sp), chrg(n_sp), mmw(n_sp), &
+    nhyd(n_sp), ncar(n_sp), n14N(n_sp), n15N(n_sp), noxy(n_sp), &
+    dtype(n_sp), ad(n_sp), sd1(n_sp), phi(n_sp), sd2(n_sp), sd3(n_sp), &
+    ibnd(n_sp,2), bval(n_sp,2))
+  
+  ! read in settings for neutrals
+  read(fid_neu,'(A)') ts_header ! column headers
+  do i_sp = 1, n_neu
+    read(fid_neu,961) tn1, &
+      sp_list(i_sp), istat(i_sp), chrg(i_sp), mmw(i_sp), &
+      nhyd(i_sp), ncar(i_sp), n14N(i_sp), n15N(i_sp), noxy(i_sp), &
+      dtype(i_sp), ad(i_sp), sd1(i_sp), phi(i_sp), sd2(i_sp), sd3(i_sp), &
+      ibnd(i_sp,1), bval(i_sp,1), ibnd(i_sp,2), bval(i_sp,2)
+  end do
+
+961 format(I4, &
+      1X, A12, 1X, I1, 1X, I2, 1X, F7.1, &
+      5I4, 2X, &
+      I4, 1X, ES9.2, 1X, F6.3, 1X, ES9.2, 1X, F5.1, 1X, F6.1, 6X, &
+      I1, 2X, ES10.3, 3X, I1, 2X, ES10.3)
+  
+  ! read in settings for ions
+  read(fid_ion,'(A)') ts_header
+  ichk = 0
+  do i_sp = n_neu+1, n_neu+n_ion
+     read(fid_ion,962) tn1, &
+      sp_list(i_sp), istat(i_sp), chrg(i_sp), mmw(i_sp), &
+      nhyd(i_sp), ncar(i_sp), n14N(i_sp), n15N(i_sp), noxy(i_sp), &
+      ibnd(i_sp,1), bval(i_sp,1), ibnd(i_sp,2), bval(i_sp,2)
+    if (istat(i_sp) == 1) ichk = ichk + 1
+  end do
+  
+962 format(I4, &
+      1X, A12, 1X, I1, 1X, I2, 1X, F7.1, &
+      5I4, 4X, &
+      I1, 2X, ES10.3, 3X, I1, 2X, ES10.3)
+
+  close(unit=fid_neu)
+  close(unit=fid_ion)
+
+  sp_list(0)    = '            ' ! for total
+  sp_list(n_sp) = 'E           ' ! electrons
+  mmw(n_sp) = zero
+
+  !----------------------------------------------------------------------------
+  !  Create lists of species in chemical and diffusive equilibrium
+  !----------------------------------------------------------------------------
+
+  ! count number of species in chemical and diffusive equilibrium
+  n_c = 0; n_d = 0
+  do i_sp = 1, n_sp-1
+    if (istat(i_sp) == 1) n_c = n_c + 1 ! species in chemical equilibrium
+    if (istat(i_sp) == 2) n_d = n_d + 1 ! species diffusive equilibrium
+  end do
+  n_chem = n_c
+  n_diff = n_d
+  
+
+  if (ichk > 0) then
+    have_ions = .true.
+    n_chem_e = n_chem + 1 ! with electrons
+  else
+    have_ions = .false.
+    n_chem_e = n_chem
+  end if
+
+  ! index mapping between lists
+  ! list of chemical species -> list of all species
+  allocate(im_chem_all(n_chem_e))
+  im_chem_all = 0
+  ! list of all species -> list of chemical species
+  allocate(im_all_chem(0:n_sp))
+  im_all_chem = 0
+  ! list of diffusing species -> list of all species
+  allocate(im_diff_all(n_diff))
+  im_diff_all = 0
+  ! list of all species -> list of diffusing species
+  allocate(im_all_diff(0:n_sp))
+  im_all_diff = 0
+
+
+  ! calculate mapping between list of diffusing species and list of all species
+  n_d = 0
+  do i_sp = 1, n_sp-1
+    if (istat(i_sp) == 2) then
+      n_d = n_d + 1
+      im_diff_all(n_d) = i_sp
+      im_all_diff(i_sp) = n_d
+    end if
+  end do
+
+  write(*,'("DIFFUSING SPECIES")')
+  if (size(im_diff_all) > 0) then
+    write(*,'(10(1X,A12))') (sp_list(im_diff_all(n_d)), n_d=1, n_diff)
+  else
+    write(*,'("  NONE")')
+  end if
+
+
+  ! calculate mapping between list of chemical species and list of all species
+  n_c = 0
+  do i_sp = 1, n_sp-1
+    if (istat(i_sp) == 1) then ! species in chemical equilibrium
+      n_c = n_c + 1
+      im_chem_all(n_c) = i_sp
+      im_all_chem(i_sp) = n_c
+    end if
+  end do
+  
+  ! entry for electrons
+  if (have_ions) then
+    im_chem_all(n_chem_e) = n_sp
+    im_all_chem(n_sp) = n_chem_e
+  end if
+
+  ! print list of chemical species to screen
+  write(*,'("CHEMICAL SPECIES")')
+  if (size(im_chem_all) > 0) then
+    write(*,'(10(1X,A12))') (sp_list(im_chem_all(n_c)), n_c=1, n_chem)
+  else
+    write(*,'("  NONE")')
+  end if
+
+  !----------------------------------------------------------------------------
+  !  Determine indices of key species
+  !----------------------------------------------------------------------------
+  
+  iN2  = find_name('N2          ', sp_list)
+  iCO2 = find_name('CO2         ', sp_list)
+  iELE = find_name('E           ', sp_list)
+
+end subroutine read_species
+
+
+subroutine read_atmos
+! This subroutine reads in the model atmosphere from atm1D.in, and calculates 
+! other atmospheric state quantities. 
+
+  !----------------------------------------------------------------------------
+  !  Modules
+  !----------------------------------------------------------------------------
+  
+  use types, only: wp => dp
+  use constants
+  use global_variables
+  use utils, only : find_name, locate
+
+  !----------------------------------------------------------------------------
+  !  Local variables
+  !----------------------------------------------------------------------------
+  
+  implicit none
+  ! file unit numbers for atm1D.in
+  integer :: fid
+  ! minimum density
+  real(wp), parameter :: den_min = 1.0e-50_wp
+  
+  ! species names in atm1D.in
+  character(len=12), allocatable, dimension(:) :: sp_atm
+  ! index mapping from sp_atm to sp_list
+  integer, allocatable, dimension(:) :: im_atm_list
+  ! does species from settings have density defined in atmosphere?
+  logical, allocatable, dimension(:) :: has_den
+  
+  ! loop variables
+  integer :: i_sp, i_z
+  ! temporary / dummy variables
+  character(len=128) :: ts_header
+  character(len=12) :: ts_name
+  integer :: tn_sp1, tn_sp2
+  
+  !----------------------------------------------------------------------------
+  !  Read in model atmosphere from atm1D
+  !----------------------------------------------------------------------------
+  
+  ! start with all species undefined, then flip when density found
+  allocate(has_den(n_sp))
+  has_den = .false.
+  
+  ! read in model atmosphere file atm1D.in
+  open(newunit=fid, file='atm1D.in', status='old', action='read')
+    
+    read(fid,*) n_z, tn_sp1
+    ! allocate variables
+    allocate(sp_atm(tn_sp1), im_atm_list(tn_sp1))
+    allocate(z(n_z), rz(0:n_z+1), grv(n_z), eK(n_z))
+    allocate(Tn(n_z), Te(n_z), prs(n_z), mass(n_z), rho(n_z))
+    allocate(den(n_z,0:n_sp), den_old(n_z,0:n_sp), vmr(n_z,n_sp))
+
+    ! read species and associate them with sp_list from species settings files
+    ! i.e., nmolecules.settings and imolecules.settings
+    read(fid,'(A)') ts_header
+    read(fid,'(10(2X,A12,1X))') (sp_atm(i_sp), i_sp=1, tn_sp1)
+    do i_sp = 1, tn_sp1
+      tn_sp2 = find_name(sp_atm(i_sp),sp_list)
+      im_atm_list(i_sp) = tn_sp2
+      if (tn_sp2 /= 0) has_den(tn_sp2) = .true.
+    end do
+
+    ! read in model atmosphere and populate relevant variables
+    read(fid,'(A)') ts_header
+    read(fid,*) (z(i_z), i_z=1, n_z)
+    read(fid,'(A)') ts_header
+    read(fid,*) (rz(i_z), i_z=1, n_z)
+    read(fid,'(A)') ts_header
+    read(fid,*) (grv(i_z), i_z=1, n_z)
+    read(fid,'(A)') ts_header
+    read(fid,*) (Tn(i_z), i_z=1, n_z)
+    read(fid,'(A)') ts_header
+    read(fid,*) (Te(i_z), i_z=1, n_z)
+    read(fid,'(A)') ts_header
+    read(fid,*) (prs(i_z), i_z=1, n_z)
+    read(fid,'(A)') ts_header
+    read(fid,*) (rho(i_z), i_z=1, n_z)
+    read(fid,'(A)') ts_header
+    read(fid,*) (mass(i_z), i_z=1, n_z)
+    read(fid,'(A)') ts_header
+    read(fid,*) (eK(i_z), i_z=1, n_z)
+    read(fid,'(A)') ts_header
+    read(fid,*) (den(i_z,0), i_z=1, n_z) ! den(*,0) is for overall density
+    do i_sp = 1, tn_sp1
+      tn_sp2 = im_atm_list(i_sp)
+      read(fid,'(A)') ts_name
+      read(fid,*) (den(i_z,tn_sp2), i_z=1, n_z)
+    end do
+    
+  close(unit=fid)
+  
+  ! altitude to opaque atmosphere layer (e.g., aerosols)
+  z_bot = 0.E5_wp
+  iz_bot = locate(z_bot, z)
+  
+  !----------------------------------------------------------------------------
+  !  Recalculate quanitites to ensure consistency
+  !----------------------------------------------------------------------------
+
+  ! apply bottom boundary condition of stipulated mole ratio
+  do i_sp = 1, n_diff
+    tn_sp2 = im_diff_all(i_sp)
+    if (ibnd(tn_sp2,1) == 3) then
+      den(1,tn_sp2) = bval(tn_sp2,1)*den(1,0)
+    end if
+  end do
+
+  ! set density to min value if species does not have density defined
+  do concurrent (i_sp = 1:n_sp-1)
+    if (.not. has_den(i_sp)) then
+      den(:,i_sp) = den_min
+      write(*,'("Density not defined for ", A, &
+        "... Assigning minimum density of ", ES8.1)') sp_list(i_sp), den_min
+    end if
+  end do
+
+  ! set density to min value if below minimum
+  where (den < den_min) den = den_min
+  
+  ! reset electron density to sum of ion densities
+  do concurrent (i_z = 1:n_z)
+    den(i_z,n_sp) = sum(den(i_z, n_neu+1:n_sp-1))
+  end do
+  
+  ! reset total density
+  do concurrent (i_z = 1:n_z)
+    den(i_z,0) = sum(den(i_z,1:n_sp))
+  end do
+  
+  ! set density to fixed mole fraction according to settings
+  do concurrent (i_sp = 1:n_sp-1)
+    if (istat(i_sp) == 3) then
+      den(:,i_sp) = bval(i_sp,1)*den(:,0)
+    end if
+  end do
+
+  ! calculate mole fraction
+  do concurrent (i_z = 1:n_z, i_sp = 1:n_sp)
+      vmr(i_z,i_sp) = den(i_z,i_sp)/den(i_z,0)
+  end do
+
+  ! reset gravity
+  do concurrent (i_z = 1:n_z)
+    grv(i_z) = GM/rz(i_z)**2
+  end do
+
+  ! reset pressure to be consistent with N & T
+  do concurrent (i_z = 1:n_z)
+    prs(i_z) = kB*Tn(i_z)*den(i_z,0)
+  end do
+
+  ! calculate mass density, mean molecular mass
+  do concurrent (i_z = 1:n_z)
+    if (n_ion > 0) then
+      rho(i_z) = dot_product(mmw, den(i_z,1:n_sp)) * amu
+      mass(i_z) = rho(i_z) / (den(i_z,0) + two * den(i_z,n_sp)) / amu !? why
+    else
+      rho(i_z) = dot_product(mmw, den(i_z,1:n_sp)) * amu
+      mass(i_z) = rho(i_z) / den(i_z,0) / amu
+    end if
+  end do
+
+  !----------------------------------------------------------------------------
+  !  Auxillary altitude arrays
+  !----------------------------------------------------------------------------
+
+  allocate(rz_mid(0:n_z), dr_mid(0:n_z), dr(n_z))
+  allocate(Ht(n_z,0:n_sp), col(n_z))
+
+  rz(0) = rz(1) - (rz(2) - rz(1))
+  rz(n_z+1) = rz(n_z) + (rz(n_z) - rz(n_z-1))
+  do concurrent (i_z = 0:n_z)
+    rz_mid(i_z) = half * (rz(i_z+1) + rz(i_z))
+    dr_mid(i_z) = rz(i_z+1) - rz(i_z)
+  end do
+
+  do concurrent (i_z = 1:n_z)
+    dr(i_z) = rz_mid(i_z) - rz_mid(i_z-1)
+  end do
+
+  !----------------------------------------------------------------------------
+  !  Scale heights
+  !----------------------------------------------------------------------------
+  
+  ! for each species
+  do concurrent (i_z = 1:n_z, i_sp = 1:n_sp-1)
+    Ht(i_z,i_sp) = kB * tn(i_z) / grv(i_z) / mmw(i_sp) / amu
+  end do
+  
+  ! mean scale height
+  do concurrent (i_z = 1:n_z)
+    Ht(i_z,0) = kB * tn(i_z) / grv(i_z) / mass(i_z) / amu
+  end do
+
+  !----------------------------------------------------------------------------
+  !  Column density above
+  !----------------------------------------------------------------------------
+
+  col(n_z) = den(n_z,0) * Ht(n_z,0)
+  do i_z = n_z-1, 1, -1
+    col(i_z) = col(i_z+1) + half * (den(i_z,0) + den(i_z+1,0)) * dr_mid(i_z)
+  end do
+
+  deallocate(sp_atm, im_atm_list, has_den)
+end subroutine read_atmos
+
 
 subroutine read_reactions
 ! This subroutine reads in the list of reactions and their rate constants in 
